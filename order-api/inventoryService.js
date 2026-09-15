6c7beb1d53bdc0ac8d776265c75dccf7bc3c9b2e
pranav0990 <pranavhire2905@gmail.com>
2026-09-15 17:30:58 +0530

    Create inventoryService.js

---
+++ order-api/inventoryService.js
@@ -0,0 +1,86 @@
+const {
+
+    BlobServiceClient
+
+} = require("@azure/storage-blob");
+
+
+require("dotenv").config();
+
+
+const blobServiceClient =
+
+    BlobServiceClient.fromConnectionString(
+
+        process.env.AZURE_STORAGE_CONNECTION_STRING
+
+    );
+
+
+const containerClient =
+
+    blobServiceClient.getContainerClient(
+
+        process.env.INVENTORY_CONTAINER
+
+    );
+
+
+const blobClient =
+
+    containerClient.getBlockBlobClient(
+
+        process.env.INVENTORY_BLOB
+
+    );
+
+
+async function streamToString(
+
+    readableStream
+
+) {
+
+    let data = "";
+
+
+    for await (const chunk of readableStream) {
+
+        data += chunk.toString();
+
+    }
+
+
+    return data;
+
+}
+
+
+async function getInventory() {
+
+
+    const response =
+
+        await blobClient.download();
+
+
+    const content =
+
+        await streamToString(
+
+            response.readableStreamBody
+
+        );
+
+
+    return JSON.parse(content);
+
+}
+
+
+module.exports = {
+
+    getInventory
+
+};
+ 
